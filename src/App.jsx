7b06b2201bc33ccf58e3f1d1f495b5ed622cb0d5
Richard <richardaurelio42@gmail.com>
2024-05-10 19:20:54 +0700

    penambahan profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home";
+import Profile from "./pages/profile";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
   },
 ]);
 
