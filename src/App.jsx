e8ab2f4bb91edb785c0dfc1d0158d75d8bb242db
Shahrizan <shahrizan.code@gmail.com>
2024-05-10 19:48:24 +0700

    add register page and services folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home";
+import Register from "./pages/register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: '/register',
+    element: <Register />
+  }
 ]);
 
 export default function App() {
---
+++ src/pages/register/index.jsx
@@ -0,0 +1,32 @@
+import { Form, redirect } from 'react-router-dom';
+
+export async function registerAction() {
+  const user = await Register();
+  redirect('/');
+}
+
+export default function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <Form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          gap: '.5rem',
+        }}
+      >
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" required />
+        <label htmlFor="image">Profile Image</label>
+        <input type="file" name="image" id="image" required />
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" required />
+        <label htmlFor="password">Password</label>
+        <input type="text" name="password" id="password" required />
+        <button type="submit">Submit</button>
+      </Form>
+    </div>
+  );
+}
