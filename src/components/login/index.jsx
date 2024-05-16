431981f049b29cf225e6b0c503b298dfe62d0453
Raafi <92612605+Raafina@users.noreply.github.com>
2024-05-16 12:02:19 +0700

    login page with login google.. without redux (#6)
    * try to pull req
    
    * styling login and added bootstrap for temp
    
    * solve conflict from main
    
    * added google login without fetching API
    
    * login with google, without fetching API
    
    ---------
    
    Co-authored-by: Raafina <raafinuradzani@gmail.com>

---
+++ src/components/login/index.jsx
@@ -0,0 +1,77 @@
+import { Button, Form, FormGroup } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { Typography } from "@mui/material";
+
+import React from "react";
+import { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { login } from "../../redux/actions/auth";
+import GoogleLogin from "../GoogleLogin";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  // const navigate = useNavigate();
+  // const dispatch = useDispatch();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // login action (fetch api)
+    // dispatch(login(navigate, email, password, setIsLoading));
+  };
+
+  return (
+    // <Form onSubmit={onSubmit}>
+    <Form>
+      <Form.Group className="mb-2" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          // value={email}
+          // onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <Form.Text className="text-muted">
+          We will never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          // value={password}
+          // onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </Form.Group>
+      <FormGroup className="text-center">
+        <Button
+          variant="primary"
+          type="submit"
+          className="w-100"
+          disabled={isLoading}
+        >
+          {isLoading ? "Authentication..." : "Login"}
+        </Button>
+      </FormGroup>
+      <Typography align="center" my={1}>
+        Or
+      </Typography>
+      <FormGroup className="text-center">
+        {/* <GoogleLogin text={"Login with Google"} /> */}
+        <GoogleLogin text="Login with Google" />
+      </FormGroup>
+      <p className=" text-center">
+        Don't have an account yet?
+        <Link as={Link} to="/register">
+          Register
+        </Link>
+      </p>
+    </Form>
+  );
+}
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import LoginComponent from "../../components/Login";
+import { Row, Col, Card } from "react-bootstrap";
 
 export default function Login() {
   return (
-    <div>
-      <h1>Hello Login</h1>
-    </div>
+    <Row className="d-flex justify-content-center align-items-center mt-5 mb-5">
+      <Col md={3}>
+        <Card>
+          <Card.Header>Login</Card.Header>
+          <Card.Body>
+            <LoginComponent />
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   );
 }
