045dce100c2b7f5aef4381ac4d795c403f3d2dd2
Shahrizan <shahrizan.code@gmail.com>
2024-05-17 19:31:17 +0700

    Login page v2 (#14)
    * try to pull req
    
    * styling login and added bootstrap for temp
    
    * solve conflict from main
    
    * added google login without fetching API
    
    * login with google, without fetching API
    
    * Fetching API
    
    * solve conflict
    
    * styling loginpage
    
    * styling login, register
    
    * styling login, register, change icon at navbar
    
    * change favicon
    
    * solve comment req
    
    ---------
    
    Co-authored-by: Raafina <raafinuradzani@gmail.com>

--- src/components/GoogleLogin/index.jsx
+++ src/components/GoogleLogin/index.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import { useGoogleLogin } from "@react-oauth/google";
-import { Button } from "react-bootstrap";
+import { Button } from "@mui/material";
 import PropTypes from "prop-types";
-// import { useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
-// import { loginWithGoogle } from "../../redux/actions/auth";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { loginWithGoogle } from "../../redux/actions/auth";
 
 const GoogleLogin = ({ text }) => {
-  //   const dispatch = useDispatch();
-  //   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const login = useGoogleLogin({
-    onSuccess: (codeResponse) => console.log(codeResponse),
+    onSuccess: (codeResponse) =>
+      dispatch(loginWithGoogle(navigate, codeResponse?.access_token)),
   });
 
   return (
     <Button
       type="button"
-      className="w-100"
-      variant="secondary"
+      size="large"
+      variant="contained"
+      fullWidth
       onClick={() => login()}
+      sx={{
+        bgcolor: "white", // Background color
+        color: "black",
+        "&:hover": {
+          bgcolor: "lightgray", // Background color on hover
+        },
+      }}
     >
       {text}
     </Button>
